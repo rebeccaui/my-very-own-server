ab3b06eef505e4efa1c7e40c9df5b95c4342022b
rwieberdink <rebeccawieberdink@gmail.com>
2018-07-31 19:40:06 -0400

    trial 1

---
+++ logic.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var PORT1 = 7000;
+var PORT2 = 7500;
+
+function handleRequest(request, response) {
+    console.log("Hey, nice dress. You're so brave for wearing something like that!");
+}
+
+var server = http.createServer(handlerRequest);
+
+server.listen(PORT1, function() {
+    console.log("Server listening on: https://localhost:%s", PORT1);
+}); 
+
+server.listen(PORT2, function() {
+    console.log("Server listening on: https://localhost:%s", PORT2);
+}); 
